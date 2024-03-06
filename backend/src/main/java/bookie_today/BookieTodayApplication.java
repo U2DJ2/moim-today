1802be3b8ac2af6aa95a2ec2850b0d01061406fd
Jung-YouHwan <yhwjd99@gmail.com>
2024-03-06 15:35:29 +0900

    [BE] feat : 초기 설정

---
+++ backend/src/main/java/bookie_today/BookieTodayApplication.java
@@ -0,0 +1,13 @@
+package bookie_today;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookieTodayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
