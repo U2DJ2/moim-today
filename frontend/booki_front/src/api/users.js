482159f571fabd95f09ea4725b3097fb5716bb66
yurim222 <yurim@ajou.ac.kr>
2024-03-07 20:12:00 +0900

    [FE] chore : 아무 파일 추가

---
+++ frontend/booki_front/src/api/users.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function users() {
+  return <div>users</div>;
+}
+
+export default users;
---
+++ frontend/booki_front/src/pages/LandingPage/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../../components/Header";
+function index() {
+  return (
+    <div>
+      <Header />
+      <div>LandingPage</div>
+    </div>
+  );
+}
+
+export default index;
