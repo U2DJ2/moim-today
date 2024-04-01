c9b83cf00b7ef74ba21f72fe7b129cc11dfeef1a
Yang JiWoong <jerry3269@ajou.ac.kr>
2024-04-01 15:31:19 +0900

    [BE] feat : 로그인 Interceptor / Argumentresolver 구현

---
+++ backend/src/main/java/booki_today/global/constant/MemberSessionConstant.java
@@ -0,0 +1,16 @@
+package booki_today.global.constant;
+
+public enum MemberSessionConstant {
+
+    MEMBER_SESSION("MemberSession");
+
+    private final String value;
+
+    MemberSessionConstant(final String value) {
+        this.value = value;
+    }
+
+    public String value() {
+        return value;
+    }
+}
