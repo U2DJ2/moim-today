43b503ace2d7c604c197ef2d8f5673f992a722f8
Yang JiWoong <jerry3269@ajou.ac.kr>
2024-04-01 15:43:20 +0900

    [BE] feat : 로그인 비니지스 로직 구현

--- backend/src/main/java/booki_today/persistence/repository/member/MemberJpaRepository.java
+++ backend/src/main/java/booki_today/persistence/repository/member/MemberJpaRepository.java
@@ -3,5 +3,9 @@ package booki_today.persistence.repository.member;
 import booki_today.persistence.entity.member.MemberJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberJpaRepository extends JpaRepository<MemberJpaEntity, Long> {
+
+    Optional<MemberJpaEntity> findByEmail(final String email);
 }
---
+++ backend/src/main/java/booki_today/presentation/auth/AuthController.java
@@ -0,0 +1,26 @@
+package booki_today.presentation.auth;
+
+import booki_today.application.auth.AuthService;
+import booki_today.dto.auth.MemberLoginRequest;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(final AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/auth")
+    public void login(@RequestBody final MemberLoginRequest memberLoginRequest,
+                      final HttpServletRequest request) {
+        authService.login(memberLoginRequest, request);
+    }
+}
