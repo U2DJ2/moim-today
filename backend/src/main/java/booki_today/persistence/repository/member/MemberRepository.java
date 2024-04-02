dc1de7e9c20175e5bcace23b337c376e1f227917
Yang JiWoong <jerry3269@ajou.ac.kr>
2024-04-02 13:00:16 +0900

    [BE] style : final 키워드 적용

--- backend/src/main/java/booki_today/persistence/repository/member/MemberRepository.java
+++ backend/src/main/java/booki_today/persistence/repository/member/MemberRepository.java
@@ -3,6 +3,6 @@ package booki_today.persistence.repository.member;
 import booki_today.persistence.entity.member.MemberJpaEntity;
 
 public interface MemberRepository {
-    void save(MemberJpaEntity entity);
+    void save(final MemberJpaEntity entity);
     MemberJpaEntity getByEmail(final String email);
 }
