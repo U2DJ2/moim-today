1eabb9c83aaff865e9bde0dd604ef4a450bc3d10
Yang JiWoong <jerry3269@ajou.ac.kr>
2024-04-11 21:16:14 +0900

    [BE] refactor : FakeInterceptor 세션 주입으로 인한 세션 추가 로직 삭제

--- backend/src/test/java/moim_today/presentation/member/MemberControllerTest.java
+++ backend/src/test/java/moim_today/presentation/member/MemberControllerTest.java
@@ -2,22 +2,18 @@ package moim_today.presentation.member;
 
 import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import moim_today.application.member.MemberService;
-import moim_today.domain.member.MemberSession;
 import moim_today.dto.member.PasswordRecoverRequest;
 import moim_today.dto.member.PasswordUpdateRequest;
 import moim_today.dto.member.ProfileUpdateRequest;
-import moim_today.fake_DB.FakeMemberSession;
 import moim_today.fake_class.member.FakeMemberService;
 import moim_today.util.ControllerTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpMethod;
-import org.springframework.mock.web.MockHttpSession;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
-import static moim_today.global.constant.MemberSessionConstant.MEMBER_SESSION;
 import static moim_today.util.TestConstant.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
@@ -96,12 +92,8 @@ class MemberControllerTest extends ControllerTest {
     void updatePassword() throws Exception {
         PasswordUpdateRequest passwordUpdateRequest = new PasswordUpdateRequest(NEW_PASSWORD.value());
         String json = objectMapper.writeValueAsString(passwordUpdateRequest);
-        MockHttpSession session = new MockHttpSession();
-        MemberSession memberSession = FakeMemberSession.createMemberSession();
-        session.setAttribute(MEMBER_SESSION.value(), objectMapper.writeValueAsString(memberSession));
 
         mockMvc.perform(patch("/api/members/password")
-                        .session(session)
                         .contentType(APPLICATION_JSON)
                         .content(json)
                 )
@@ -120,13 +112,8 @@ class MemberControllerTest extends ControllerTest {
     @DisplayName("프로필을 조회에 성공한다.")
     @Test
     void getMemberProfile() throws Exception {
-        MockHttpSession session = new MockHttpSession();
-        MemberSession memberSession = FakeMemberSession.createMemberSession();
-        session.setAttribute(MEMBER_SESSION.value(), objectMapper.writeValueAsString(memberSession));
 
-        mockMvc.perform(get("/api/members/profile")
-                        .session(session)
-                )
+        mockMvc.perform(get("/api/members/profile"))
                 .andExpect(status().isOk())
                 .andDo(document("프로필 조회 성공",
                         resource(ResourceSnippetParameters.builder()
@@ -149,14 +136,10 @@ class MemberControllerTest extends ControllerTest {
     @DisplayName("프로필을 정보를 수정한다.")
     @Test
     void updateProfile() throws Exception {
-        MockHttpSession session = new MockHttpSession();
-        MemberSession memberSession = FakeMemberSession.createMemberSession();
-        session.setAttribute(MEMBER_SESSION.value(), objectMapper.writeValueAsString(memberSession));
         ProfileUpdateRequest profileUpdateRequest = new ProfileUpdateRequest(1L);
         String json = objectMapper.writeValueAsString(profileUpdateRequest);
 
         mockMvc.perform(patch("/api/members/profile")
-                        .session(session)
                         .contentType(APPLICATION_JSON)
                         .content(json)
                 )
@@ -176,14 +159,13 @@ class MemberControllerTest extends ControllerTest {
     @DisplayName("프로필 이미지를 수정하면 회원의 프로필 이미지 URL 정보가 변경된다.")
     @Test
     void updateProfileImage() throws Exception {
-        //given
         MockMultipartFile file = new MockMultipartFile(
                 "file",
                 "filename.txt",
                 TEXT_PLAIN_VALUE,
-                "content".getBytes());
+                "content".getBytes()
+        );
 
-        //when & then
         mockMvc.perform(MockMvcRequestBuilders
                         .multipart(HttpMethod.PATCH, "/api/members/profile-image")
                         .file(file)
