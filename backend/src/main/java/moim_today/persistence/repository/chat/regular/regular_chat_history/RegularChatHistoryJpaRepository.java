4a6873136972693702adaa018c224b251a384d2b
Jung-YouHwan <yhwjd99@gmail.com>
2024-04-15 13:42:20 +0900

    [BE] refactor : 채팅 기능 관련 테이블 삭제

--- backend/src/main/java/moim_today/persistence/repository/chat/regular/regular_chat_history/RegularChatHistoryJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package moim_today.persistence.repository.chat.regular.regular_chat_history;
-
-import moim_today.persistence.entity.chat.regular.regular_chat_history.RegularChatHistoryJpaEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegularChatHistoryJpaRepository extends JpaRepository<RegularChatHistoryJpaEntity, Long> {
-}
--- backend/src/main/java/moim_today/persistence/repository/chat/single/single_chat_history/SingleChatHistoryJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package moim_today.persistence.repository.chat.single.single_chat_history;
-
-import moim_today.persistence.entity.chat.single.single_chat_history.SingleChatHistoryJpaEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SingleChatHistoryJpaRepository extends JpaRepository<SingleChatHistoryJpaEntity, Long> {
-}
