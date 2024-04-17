e961c4c3f19ccbc2574613ec8126533e8ef47bbb
Jung-YouHwan <yhwjd99@gmail.com>
2024-04-17 17:22:08 +0900

    [BE] feat : 시간표 요청 입력 시간 예외 처리

--- backend/src/main/java/moim_today/dto/schedule/TimeTableRequest.java
+++ backend/src/main/java/moim_today/dto/schedule/TimeTableRequest.java
@@ -1,10 +1,20 @@
 package moim_today.dto.schedule;
 
+import moim_today.global.error.BadRequestException;
+
 import java.time.LocalDate;
+
+import static moim_today.global.constant.exception.EveryTimeExceptionConstant.TIME_INPUT_ERROR;
 
 public record TimeTableRequest(
         String everytimeId,
         LocalDate startDate,
         LocalDate endDate
 ) {
+
+    public TimeTableRequest {
+        if(startDate.isAfter(endDate)) {
+            throw new BadRequestException(TIME_INPUT_ERROR.value());
+        }
+    }
 }
