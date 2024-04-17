6e9fd66fd320e73e1627bcaeee15cbb116c39534
Yang JiWoong <jerry3269@ajou.ac.kr>
2024-04-17 12:22:55 +0900

    [BE] feat : 로그아웃 세션 만료 구현

--- backend/src/main/java/moim_today/presentation/auth/AuthController.java
+++ backend/src/main/java/moim_today/presentation/auth/AuthController.java
@@ -1,12 +1,11 @@
 package moim_today.presentation.auth;
 
 import moim_today.application.auth.AuthService;
+import moim_today.domain.member.MemberSession;
 import moim_today.dto.auth.MemberLoginRequest;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import moim_today.global.annotation.Login;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api")
 @RestController
@@ -22,5 +21,10 @@ public class AuthController {
     public void login(@RequestBody final MemberLoginRequest memberLoginRequest,
                       final HttpServletRequest request) {
         authService.login(memberLoginRequest, request);
+    }
+
+    @PostMapping("/logout")
+    public void logout(@Login final MemberSession memberSession, final HttpServletRequest request) {
+        authService.logout(request);
     }
 }
