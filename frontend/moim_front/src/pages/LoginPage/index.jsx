76b499410e54e67354e19dbc473f4f9a57b268f8
yurim222 <yurim@ajou.ac.kr>
2024-04-23 00:13:31 +0900

    [FE] feat : 오른쪽 사진 크기 조정

--- frontend/moim_front/src/pages/LoginPage/index.jsx
+++ frontend/moim_front/src/pages/LoginPage/index.jsx
@@ -4,9 +4,11 @@ import AuthLeft from "../../components/AuthLeft";
 import right1 from "../../assets/svg/right1.svg";
 function LoginPage() {
   return (
-    <div className="flex min-h-screen w-full py-0 overflow-hidden relative ">
+    <div className="flex min-h-screen w-full py-0 overflow-hidden relative space-x-2 gap-1">
       <AuthLeft className="flex flex-col pl-36" />
-      <img src={right1} />
+      <div className="absolute right-1 top-0 h-full">
+        <img src={right1} className="object-cover h-full" />
+      </div>
     </div>
   );
 }
