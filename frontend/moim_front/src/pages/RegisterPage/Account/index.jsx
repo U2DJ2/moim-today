da6f2ba4e4374c24c1a8571c34731e555f860335
yurim222 <yurim@ajou.ac.kr>
2024-04-28 14:03:43 +0900

    [FE] feat : 회원가입페이지 이메일 및 비번 입력

---
+++ frontend/moim_front/src/pages/RegisterPage/Account/index.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import AuthLeft from "../../../components/AuthLeft";
+import AuthTitle from "../../../components/Authentification/AuthTitle";
+import { useForm } from "react-hook-form";
+function Account() {
+  const { register, handleSubmit, errors, formState, setValue } = useForm();
+  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  return (
+    <div className="flex-1 flex flex-col items-start justify-center w-96 gap-16">
+      <AuthTitle
+        className="flex flex-col w-full"
+        title={"Register"}
+        firstContent={"모임 투데이에 온 것을 환영합니다!"}
+        secondContent={"회원가입을 위해 이메일과 비밀번호를 입력해주세요."}
+        white={true}
+      />
+      <div className="flex flex-col gap-12">
+        <div className="gap-1">
+          <p className=" font-Pretendard_Black block text-xl text-white">
+            Email
+          </p>
+          <input
+            type="email"
+            name="email"
+            placeholder="enter your email"
+            className={`border-b border-white font-Pretendard_Light text-white text-xl pt-2 pb-2 bg-scarlet focus:outline-none w-full block placeholder:text-white`}
+            {...register("email", { required: true, pattern: emailPattern })}
+          />
+        </div>
+        <div className="gap-1">
+          <p className=" font-Pretendard_Black block text-xl text-white">
+            Password
+          </p>
+          <input
+            type="password"
+            name="password"
+            placeholder="enter your password"
+            className={`border-b border-white font-Pretendard_Light text-white text-xl pt-2 pb-2 bg-scarlet focus:outline-none w-full block mb-12 placeholder:text-white `}
+            {...register("password", {
+              required: true,
+            })}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Account;
--- frontend/moim_front/src/pages/RegisterPage/index.jsx
+++ frontend/moim_front/src/pages/RegisterPage/index.jsx
@@ -1,23 +1,18 @@
 import React from "react";
-import RegisterLayout from "../../components/PageContainer/RegisterContainer";
-import AuthLeft from "../../components/AuthLeft";
-import AuthTitle from "../../components/Authentification/AuthTitle";
 import AuthRight from "../../components/AuthRight";
+import Account from "./Account";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 function RegisterPage() {
   const [step, setStep] = useState(0);
   const [activeNext, setActiveNext] = useState(false);
+  const { register, handleSubmit, errors, formState, setValue } = useForm();
   return (
     <div className="flex min-h-screen w-full py-0 overflow-hidden relative gap-1 pl-28 bg-scarlet">
-      {
-        <AuthLeft
-          className="flex flex-col w-full"
-          title={"Register"}
-          firstContent={"모임 투데이에 온 것을 환영합니다!"}
-          secondContent={"이메일과 비밀번호를 입력해주세요."}
-          white={true}
-        />
-      }
+      {step === 0 ? (
+        <Account register={register} errors={errors} setValue={setValue} />
+      ) : null}
+
       <AuthRight textColor={"black"} cardColor={"white"} />
     </div>
   );
