8a083b14fd5dfd68f5363c6582db5d75307fd26a
Donghyun Kim <ilovekdh1208@ajou.ac.kr>
2024-04-30 19:38:07 +0900

    [BE] refactor : 대학교 이름 컬렉션 조회 최적화

--- backend/src/main/java/moim_today/persistence/repository/university/UniversityRepositoryImpl.java
+++ backend/src/main/java/moim_today/persistence/repository/university/UniversityRepositoryImpl.java
@@ -2,11 +2,17 @@ package moim_today.persistence.repository.university;
 
 import moim_today.global.error.NotFoundException;
 import moim_today.persistence.entity.university.UniversityJpaEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static java.util.Collections.nCopies;
+import static moim_today.global.constant.UniversityConstant.UNIVERSITY_EMAIL;
+import static moim_today.global.constant.UniversityConstant.UNIVERSITY_NAME;
 import static moim_today.global.constant.exception.UniversityExceptionConstant.UNIVERSITY_EMAIL_NOT_FOUND;
 import static moim_today.global.constant.exception.UniversityExceptionConstant.UNIVERSITY_NOT_FOUND;
 
@@ -14,9 +20,11 @@ import static moim_today.global.constant.exception.UniversityExceptionConstant.U
 public class UniversityRepositoryImpl implements UniversityRepository {
 
     private final UniversityJpaRepository universityJpaRepository;
+    private final JdbcTemplate jdbcTemplate;
 
-    public UniversityRepositoryImpl(final UniversityJpaRepository universityJpaRepository) {
+    public UniversityRepositoryImpl(final UniversityJpaRepository universityJpaRepository, JdbcTemplate jdbcTemplate) {
         this.universityJpaRepository = universityJpaRepository;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
@@ -52,7 +60,28 @@ public class UniversityRepositoryImpl implements UniversityRepository {
     }
 
     @Override
+    public List<UniversityJpaEntity> findExistingUniversities(final List<String> universityNames) {
+        if (universityNames == null || universityNames.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        String inSql = String.join(",", nCopies(universityNames.size(), "?"));
+        String sql = "SELECT * FROM university WHERE "+UNIVERSITY_NAME.value()+" IN (" + inSql + ")";
+
+        return jdbcTemplate.query(sql, universityNames.toArray(new Object[0]), universityJpaEntityRowMapper());
+    }
+
+    @Override
     public Optional<UniversityJpaEntity> findById(final long id) {
         return universityJpaRepository.findById(id);
+    }
+
+    private RowMapper<UniversityJpaEntity> universityJpaEntityRowMapper() {
+        return (rs, rowNum) -> {
+            return UniversityJpaEntity.builder()
+                    .universityName(rs.getString(UNIVERSITY_NAME.value()))
+                    .universityEmail(rs.getString(UNIVERSITY_EMAIL.value()))
+                    .build();
+        };
     }
 }
