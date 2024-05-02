e31f84d32fd331074e8119f2a1b4fd41fece4ca5
KIM JUN YOUNG <tjclear214@gmail.com>
2024-05-02 20:55:35 +0900

    [FE] feat : Personal Calendar 페이지 추가

--- frontend/moim_front/src/components/Header/index.jsx
+++ frontend/moim_front/src/components/Header/index.jsx
@@ -25,8 +25,16 @@ function Header() {
     });
   };
 
-  const handleOverview = () => {
+  const handleManage = () => {
     navigate('/manage', {
+      state: {
+        componentName: "overview",
+      },
+    });
+  };
+
+  const handleSchedule = () => {
+    navigate('/schedule', {
       state: {
         componentName: "overview",
       },
@@ -36,14 +44,14 @@ function Header() {
   return (
     <header className="flex gap-5 justify-between px-10 py-6 w-full text-lg leading-7 text-center text-gray-500 max-md:flex-wrap max-md:px-5 max-md:max-w-full">
       <nav className="flex gap-5 justify-center my-auto font-Pretendard_SemiBold cursor-pointer">
-        <p onClick={handleOverview}>
+        <p onClick={handleManage}>
           대시보드
         </p>
         <ModalTest isOpen={isModalOpen} closeModal={closeModal} />
         <a href="/creation">모임 생성</a>
       </nav>
       <div className="flex gap-5 justify-center font-Pretendard_SemiBold whitespace-nowrap cursor-pointer">
-        <div className="justify-center my-auto">아주대학교</div>
+        <div className="justify-center my-auto" onClick={handleSchedule}>아주대학교</div>
         <img
           onClick={handleProfile}
           src="https://cdn.builder.io/api/v1/image/assets/TEMP/c9e3048d2ecf88eda8221e80e1f3587f63212f6776bfaae61a153b6c70100a59?apiKey=d805a42ceca34cfc9ccedfe9a24c9a43&"
