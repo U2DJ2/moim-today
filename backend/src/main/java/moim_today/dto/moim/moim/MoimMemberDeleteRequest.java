c50c7b32dd3d64c9182e15cd688b6e76a2382e8b
Donghyun Kim <ilovekdh1208@ajou.ac.kr>
2024-05-06 13:26:50 +0900

    [BE] refactor : record 중복 메서드 삭제

--- backend/src/main/java/moim_today/dto/moim/moim/MoimMemberDeleteRequest.java
+++ backend/src/main/java/moim_today/dto/moim/moim/MoimMemberDeleteRequest.java
@@ -7,10 +7,4 @@ public record MoimMemberDeleteRequest (
         long moimId,
         long memberId
 ){
-    public long moimId(){
-        return moimId;
-    }
-    public long memberId(){
-        return memberId;
-    }
 }
