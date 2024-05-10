b017f8367df7b060d97c539d5883aa7cd620ad73
yurim222 <yurim@ajou.ac.kr>
2024-05-10 20:22:49 +0900

    [FE] style : 왼쪽 오른쪽 컴포넌트 크기 변경

--- frontend/moim_front/src/pages/MoimDetailPage/index.jsx
+++ frontend/moim_front/src/pages/MoimDetailPage/index.jsx
@@ -5,25 +5,27 @@ import DetailedRight from "../../components/DetailedRight";
 
 function MoimDetailPage() {
   return (
-    <div className="bg-gradient-to-b from-white to-[#F6F8FE] h-screen ">
-      <div className="flex gap-9 pt-2 flex-1 overflow-auto">
-        <DetailedLeft
-          userName={"김유림"}
-          title={"컴구 스터디 구합니다"}
-          currentCount={7}
-          capacity={10}
-          joined={false}
-        />
-        <DetailedRight
-          category={"스터디"}
-          title={"컴구 스터디 구합니다"}
-          currentCount={7}
-          capacity={10}
-          contents={
-            "안녕하세요! 아주대학교 소프트웨어학과 20학번 김유림입니다. 컴퓨터 구조 공부를 하고 있는데, 혼자하니까 의지박약으로 쉽지 않네요...함께 열심히 공부하실 분 구합니다!"
-          }
-          className={"pl-3"}
-        />
+    <div className="bg-gradient-to-b  justify-center from-white to-[#F6F8FE] h-screen w-full min-h-[800px]">
+      <div className=" flex w-fit m-auto">
+        <div className="flex gap-9 pt-2 flex-1 overflow-auto">
+          <DetailedLeft
+            userName={"김유림"}
+            title={"컴구 스터디 구합니다"}
+            currentCount={7}
+            capacity={10}
+            joined={false}
+          />
+          <DetailedRight
+            category={"스터디"}
+            title={"컴구 스터디 구합니다"}
+            currentCount={7}
+            capacity={10}
+            contents={
+              "안녕하세요! 아주대학교 소프트웨어학과 20학번 김유림입니다. 컴퓨터 구조 공부를 하고 있는데, 혼자하니까 의지박약으로 쉽지 않네요...함께 열심히 공부하실 분 구합니다!"
+            }
+            className={"pl-3"}
+          />
+        </div>
       </div>
     </div>
   );
