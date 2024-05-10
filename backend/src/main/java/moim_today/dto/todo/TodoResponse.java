41b77fa037a637948e4466380ea2d0687ff85bb4
Donghyun Kim <ilovekdh1208@ajou.ac.kr>
2024-05-10 14:49:48 +0900

    [BE] fix : 투두 설정 시간이 같은 경우에도 오류가 안 나도록 수정

--- backend/src/main/java/moim_today/dto/todo/TodoResponse.java
+++ backend/src/main/java/moim_today/dto/todo/TodoResponse.java
@@ -1,10 +1,15 @@
 package moim_today.dto.todo;
 
 import lombok.Builder;
+import moim_today.domain.todo.enums.TodoProgress;
 import moim_today.persistence.entity.todo.TodoJpaEntity;
 
 @Builder
 public record TodoResponse(
+        long todoId,
+        long memberId,
+        String contents,
+        TodoProgress todoProgress
 
 ) {
     public static TodoResponse of(final TodoJpaEntity t) {
