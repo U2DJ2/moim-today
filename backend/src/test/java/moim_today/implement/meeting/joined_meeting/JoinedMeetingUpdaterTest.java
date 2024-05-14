b150c03c4e56448d3a4c4a955b6e0daeb6d89487
Jung-YouHwan <yhwjd99@gmail.com>
2024-05-14 18:38:35 +0900

    [BE] refactor : TestConstant static import 추가

--- backend/src/test/java/moim_today/implement/meeting/joined_meeting/JoinedMeetingUpdaterTest.java
+++ backend/src/test/java/moim_today/implement/meeting/joined_meeting/JoinedMeetingUpdaterTest.java
@@ -2,12 +2,11 @@ package moim_today.implement.meeting.joined_meeting;
 
 import moim_today.persistence.entity.meeting.joined_meeting.JoinedMeetingJpaEntity;
 import moim_today.util.ImplementTest;
-import moim_today.util.TestConstant;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static moim_today.util.TestConstant.*;
 import static org.assertj.core.api.Assertions.*;
 
 
@@ -20,8 +19,8 @@ class JoinedMeetingUpdaterTest extends ImplementTest {
     @Test
     void updateAttendance() {
         // given
-        long memberId = TestConstant.MEMBER_ID.longValue();
-        long meetingId = TestConstant.MEETING_ID.longValue();
+        long memberId = MEMBER_ID.longValue();
+        long meetingId = MEETING_ID.longValue();
         boolean beforeAttendanceStatus = false;
         boolean newAttendanceStatus = true;
 
@@ -47,8 +46,8 @@ class JoinedMeetingUpdaterTest extends ImplementTest {
     @Test
     void updateUpcomingNoticeSent() {
         // given
-        long memberId = TestConstant.MEMBER_ID.longValue();
-        long meetingId = TestConstant.MEETING_ID.longValue();
+        long memberId = MEMBER_ID.longValue();
+        long meetingId = MEETING_ID.longValue();
         boolean beforeUpcomingNoticeSent = false;
         boolean afterUpcomingNoticeSent = true;
 
