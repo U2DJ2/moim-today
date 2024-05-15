47b5a1bf7cd738f92ae75c66cccdf2a7579290f8
Donghyun Kim <ilovekdh1208@ajou.ac.kr>
2024-05-16 02:26:23 +0900

    [BE] refactor : 멤버 회원가입 data format 1글자도 포함하도록 수정

--- backend/src/main/java/moim_today/dto/auth/MemberSignUpRequest.java
+++ backend/src/main/java/moim_today/dto/auth/MemberSignUpRequest.java
@@ -1,5 +1,6 @@
 package moim_today.dto.auth;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -19,13 +20,16 @@ public record MemberSignUpRequest(
         @NotBlank(message = NO_PASSWORD) String password,
         @NotBlank(message = NO_USERNAME) String username,
         @NotBlank String studentId,
-        @NotNull LocalDate birthDate,
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-M-d", timezone = "Asia/Seoul")
+        @NotNull(message = NO_BIRTH_DATE_ERROR) LocalDate birthDate,
         @NotNull(message = NO_GENDER_ENUM_ERROR) Gender gender
 ) {
     public static final String NO_PASSWORD = "패스워드가 없습니다.";
     public static final String NO_USERNAME = "사용자 이름이 없습니다.";
     public static final String NO_EMAIL = "이메일이 없습니다.";
     public static final String INVALID_EMAIL_FORMAT = "이메일 형식이 올바르지 않습니다.";
+    public static final String NO_BIRTH_DATE_ERROR = "birthDate 값이 없습니다.";
     public static final String NO_GENDER_ENUM_ERROR = "GENDER 값이 없습니다.";
 
     public MemberJpaEntity toEntity(final String encodedPassword) {
