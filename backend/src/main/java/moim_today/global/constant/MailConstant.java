f8890341361194ff4b5b9a35ff2771dfbcbc8595
Jung-YouHwan <yhwjd99@gmail.com>
2024-05-16 20:16:50 +0900

    [BE] refactor : 스케줄 색상 변경

--- backend/src/main/java/moim_today/global/constant/MailConstant.java
+++ backend/src/main/java/moim_today/global/constant/MailConstant.java
@@ -8,7 +8,7 @@ public enum MailConstant {
     UPCOMING_MEETING_NOTICE_MAIL("upcomingMeetingNoticeMail.html"),
     CERTIFICATION_PAGE("certificationPage"),
 
-    EMAIL_CERTIFICATION_COMPLETE("이메일 인증이 완료되었습니다. 이전 페이지에서 인증 절차를 마무리해주세요."),
+    EMAIL_CERTIFICATION_COMPLETE("이메일 인증이 완료되었습니다. <br> 이전 페이지에서 인증 절차를 마무리해주세요."),
     EMAIL_CERTIFICATION_FAIL("이메일 인증 링크가 만료되었습니다. 인증을 다시 시도해주세요."),
     PASSWORD_CERTIFICATION_FAIL("비밀번호 찾기 링크가 만료되었습니다. 다시 시도해주세요."),
 
