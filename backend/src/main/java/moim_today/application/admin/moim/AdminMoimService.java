6b36b8c9bb6639a1601ab1928fa46462f21cfbbc
Jung-YouHwan <yhwjd99@gmail.com>
2024-05-30 20:31:56 +0900

    [BE] feat : 모임 삭제 - 어드민

--- backend/src/main/java/moim_today/application/admin/moim/AdminMoimService.java
+++ backend/src/main/java/moim_today/application/admin/moim/AdminMoimService.java
@@ -1,11 +1,15 @@
 package moim_today.application.admin.moim;
 
 import moim_today.dto.moim.moim.MoimSimpleResponse;
+import moim_today.global.error.ForbiddenException;
+import moim_today.persistence.entity.moim.moim.MoimJpaEntity;
 import moim_today.persistence.repository.moim.moim.MoimRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+import static moim_today.global.constant.exception.MoimExceptionConstant.MOIM_FORBIDDEN_ERROR;
 
 @Service
 public class AdminMoimService {
@@ -19,5 +23,16 @@ public class AdminMoimService {
     @Transactional(readOnly = true)
     public List<MoimSimpleResponse> findAllByUniversityId(final long universityId) {
         return moimRepository.findAllByUniversityId(universityId);
+    }
+
+    @Transactional
+    public void deleteMoim(final long universityId, final long moimId) {
+        MoimJpaEntity moimJpaEntity = moimRepository.getById(moimId);
+
+        if (moimJpaEntity.getUniversityId() != universityId) {
+            throw new ForbiddenException(MOIM_FORBIDDEN_ERROR.message());
+        }
+
+        moimRepository.deleteById(moimId);
     }
 }
--- backend/src/main/java/moim_today/presentation/admin/moim/AdminMoimController.java
+++ backend/src/main/java/moim_today/presentation/admin/moim/AdminMoimController.java
@@ -3,9 +3,7 @@ package moim_today.presentation.admin.moim;
 import moim_today.application.admin.moim.AdminMoimService;
 import moim_today.dto.moim.moim.MoimSimpleResponse;
 import moim_today.global.response.CollectionResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +22,11 @@ public class AdminMoimController {
     public CollectionResponse<List<MoimSimpleResponse>> findAllByUniversityId(final long universityId) {
         List<MoimSimpleResponse> moimSimpleResponses = adminMoimService.findAllByUniversityId(universityId);
         return CollectionResponse.from(moimSimpleResponses);
+    }
+
+    // todo AdminSession 적용
+    @DeleteMapping("/moims/{moimId}")
+    public void deleteMoim(final long universityId, @PathVariable final long moimId) {
+        adminMoimService.deleteMoim(universityId, moimId);
     }
 }
