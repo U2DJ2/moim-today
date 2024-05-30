3cf37fa0d6f2db62c683aed90d8d080cc3848ce0
yurim222 <yurim@ajou.ac.kr>
2024-05-30 20:22:08 +0900

    [FE] feat : meeting Detail Page 시간 정보 및 텍스트 크기 변경

--- frontend/moim_front/src/pages/MeetingDetailPage/index.jsx
+++ frontend/moim_front/src/pages/MeetingDetailPage/index.jsx
@@ -8,6 +8,7 @@ import content from "../../assets/svg/comment_duotone.svg";
 import ContentIndex from "./ContentIndex";
 import infoIcon from "../../assets/svg/Info_duotone_line.svg";
 import CardBtn from "../MoimJoinPage/CardComponent/CardBtn";
+import formatDate from "../../utils/formatDate";
 import axios from "axios";
 function MettingDetailPage() {
   const [meetingInfo, setMeetingInfo] = useState([]);
@@ -96,8 +97,28 @@ function MettingDetailPage() {
     checkIsMember();
   }, []);
 
+  const dateConverter = (dateString) => {
+    const isoString = dateString.replace(" ", "T");
+
+    const date = new Date(isoString);
+
+    // 날짜 부분 포맷팅
+    const optionsDate = { year: "numeric", month: "long", day: "numeric" };
+    const formattedDate = new Intl.DateTimeFormat("ko-KR", optionsDate).format(
+      date
+    );
+
+    // 시간 부분 포맷팅
+    const optionsTime = { hour: "numeric", hour12: true };
+    const formattedTime = new Intl.DateTimeFormat("ko-KR", optionsTime).format(
+      date
+    );
+
+    // 날짜와 시간 결합
+    return `${formattedDate} ${formattedTime}`;
+  };
   const ContentSection = ({ image, indexName, children }) => (
-    <div className="grid grid-4 gap-2 font-Pretendard_Light text-2xl font-light">
+    <div className="grid grid-4 gap-2 font-Pretendard_Light text-xl font-light">
       <ContentIndex image={image} indexName={indexName} />
       {children}
     </div>
@@ -125,7 +146,8 @@ function MettingDetailPage() {
         </div>
         <hr />
         <ContentSection image={alarm} indexName="미팅 시간정보">
-          {meetingInfo.startDateTime} ~ {meetingInfo.endDateTime}
+          {meetingInfo.startDateTime && formatDate(meetingInfo.startDateTime)} ~
+          {meetingInfo.endDateTime && formatDate(meetingInfo.endDateTime)}
         </ContentSection>
         <ContentSection image={pin} indexName={"장소"}>
           {meetingInfo.place}
