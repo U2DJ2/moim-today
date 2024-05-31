f054987e9bd4deab95cce738a42fcf1910c8c6ab
Jung-YouHwan <yhwjd99@gmail.com>
2024-05-31 23:06:56 +0900

    [BE] feat : 스케줄링 설정 추가

---
+++ backend/src/main/java/moim_today/global/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package moim_today.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@Configuration
+public class SchedulingConfig {
+}
