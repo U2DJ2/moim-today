6d2b182ffbbfa8c7a8a8532a10c4eb50f419e0a2
Jung-YouHwan <yhwjd99@gmail.com>
2024-06-01 10:29:38 +0900

    [BE] test : 미팅 삭제 - 어드민

--- backend/src/test/java/moim_today/application/admin/meeting/AdminMeetingServiceTest.java
+++ backend/src/test/java/moim_today/application/admin/meeting/AdminMeetingServiceTest.java
@@ -1,10 +1,13 @@
 package moim_today.application.admin.meeting;
 
 import moim_today.dto.admin.meeting.AdminMeetingResponse;
+import moim_today.global.error.ForbiddenException;
 import moim_today.persistence.entity.meeting.meeting.MeetingJpaEntity;
 import moim_today.persistence.entity.moim.moim.MoimJpaEntity;
 import moim_today.persistence.repository.meeting.meeting.MeetingRepository;
 import moim_today.persistence.repository.moim.moim.MoimRepository;
+import moim_today.util.DatabaseCleaner;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +15,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
+import static moim_today.global.constant.exception.MeetingExceptionConstant.MEETING_FORBIDDEN_ERROR;
+import static moim_today.util.TestConstant.*;
 import static org.assertj.core.api.Assertions.*;
 
 
@@ -26,6 +31,14 @@ class AdminMeetingServiceTest {
 
     @Autowired
     private MeetingRepository meetingRepository;
+
+    @Autowired
+    private DatabaseCleaner databaseCleaner;
+
+    @BeforeEach
+    void setUpDatabase() {
+        databaseCleaner.cleanUp();
+    }
 
     @DisplayName("모임 id에 맞는 미팅 정보를 찾는다.")
     @Test
@@ -58,5 +71,55 @@ class AdminMeetingServiceTest {
 
         // then
         assertThat(adminMeetingResponses.size()).isEqualTo(2);
+    }
+
+    @DisplayName("어드민 권한으로 미팅을 삭제한다.")
+    @Test
+    void deleteMeeting() {
+        // given 1
+        MoimJpaEntity moimJpaEntity = MoimJpaEntity.builder()
+                .universityId(UNIV_ID.longValue())
+                .build();
+
+        moimRepository.save(moimJpaEntity);
+
+        // given 2
+        MeetingJpaEntity meetingJpaEntity = MeetingJpaEntity.builder()
+                .moimId(moimJpaEntity.getId())
+                .build();
+
+        meetingRepository.save(meetingJpaEntity);
+
+        // when
+        adminMeetingService.deleteMeeting(UNIV_ID.longValue(), meetingJpaEntity.getId());
+
+        // then
+        assertThat(meetingRepository.count()).isEqualTo(0);
+    }
+
+    @DisplayName("해당 학교의 관리자가 아니면 미팅을 삭제할 수 없다.")
+    @Test
+    void deleteMeetingForbidden() {
+        // given 1
+        long universityId = UNIV_ID.longValue();
+        long otherUniversityId = UNIV_ID.longValue() + 1;
+
+        MoimJpaEntity moimJpaEntity = MoimJpaEntity.builder()
+                .universityId(universityId)
+                .build();
+
+        moimRepository.save(moimJpaEntity);
+
+        // given 2
+        MeetingJpaEntity meetingJpaEntity = MeetingJpaEntity.builder()
+                .moimId(moimJpaEntity.getId())
+                .build();
+
+        meetingRepository.save(meetingJpaEntity);
+
+        // expected
+        assertThatThrownBy(() -> adminMeetingService.deleteMeeting(otherUniversityId, meetingJpaEntity.getId()))
+                .isInstanceOf(ForbiddenException.class)
+                .hasMessage(MEETING_FORBIDDEN_ERROR.message());
     }
 }
