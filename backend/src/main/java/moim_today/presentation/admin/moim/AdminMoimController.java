a07f4dd9983b4cfd03b6f3ef6f3a31e574ea875e
Jung-YouHwan <yhwjd99@gmail.com>
2024-06-02 10:21:18 +0900

    [BE] refactor : 어드민 모임, 미팅 AdminSession 적용

--- backend/src/main/java/moim_today/presentation/admin/moim/AdminMoimController.java
+++ backend/src/main/java/moim_today/presentation/admin/moim/AdminMoimController.java
@@ -1,7 +1,9 @@
 package moim_today.presentation.admin.moim;
 
 import moim_today.application.admin.moim.AdminMoimService;
+import moim_today.domain.university.AdminSession;
 import moim_today.dto.moim.moim.MoimSimpleResponse;
+import moim_today.global.annotation.Login;
 import moim_today.global.response.CollectionResponse;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,16 +19,14 @@ public class AdminMoimController {
         this.adminMoimService = adminMoimService;
     }
 
-    // todo AdminSession 적용
     @GetMapping("/moims")
-    public CollectionResponse<List<MoimSimpleResponse>> findAllByUniversityId(final long universityId) {
-        List<MoimSimpleResponse> moimSimpleResponses = adminMoimService.findAllByUniversityId(universityId);
+    public CollectionResponse<List<MoimSimpleResponse>> findAllByUniversityId(@Login final AdminSession adminSession) {
+        List<MoimSimpleResponse> moimSimpleResponses = adminMoimService.findAllByUniversityId(adminSession.universityId());
         return CollectionResponse.from(moimSimpleResponses);
     }
 
-    // todo AdminSession 적용
     @DeleteMapping("/moims/{moimId}")
-    public void deleteMoim(final long universityId, @PathVariable final long moimId) {
-        adminMoimService.deleteMoim(universityId, moimId);
+    public void deleteMoim(@Login final AdminSession adminSession, @PathVariable final long moimId) {
+        adminMoimService.deleteMoim(adminSession.universityId(), moimId);
     }
 }
