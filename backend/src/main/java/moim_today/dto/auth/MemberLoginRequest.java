e5e357de728a6d0ce093151aa767dd47b50eb340
Yang JiWoong <jerry3269@ajou.ac.kr>
2024-06-03 14:20:31 +0900

    [BE] feat : 회원 인증 요청 Validation

--- backend/src/main/java/moim_today/dto/auth/MemberLoginRequest.java
+++ backend/src/main/java/moim_today/dto/auth/MemberLoginRequest.java
@@ -1,8 +1,16 @@
 package moim_today.dto.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record MemberLoginRequest(
-        String email,
-        String password,
+        @Email(message = INVALID_EMAIL_FORMAT) @NotBlank(message = EMAIL_BLANK_ERROR) String email,
+        @NotBlank(message = PASSWORD_BLANK_ERROR) String password,
         boolean isKeepLogin
 ) {
+
+    private static final String EMAIL_BLANK_ERROR = "이메일은 공백일 수 없습니다.";
+    private static final String INVALID_EMAIL_FORMAT = "이메일 형식이 올바르지 않습니다.";
+    private static final String PASSWORD_BLANK_ERROR = "비밀번호는 공백일 수 없습니다.";
+
 }
--- backend/src/main/java/moim_today/dto/auth/MemberSignUpRequest.java
+++ backend/src/main/java/moim_today/dto/auth/MemberSignUpRequest.java
@@ -2,6 +2,7 @@ package moim_today.dto.auth;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
@@ -14,23 +15,26 @@ import static moim_today.global.constant.MemberConstant.DEFAULT_PROFILE_URL;
 
 @Builder
 public record MemberSignUpRequest(
-        long universityId,
-        long departmentId,
-        @Email(message = INVALID_EMAIL_FORMAT) @NotBlank(message = NO_EMAIL) String email,
-        @NotBlank(message = NO_PASSWORD) String password,
-        @NotBlank(message = NO_USERNAME) String username,
-        @NotBlank String studentId,
+        @Min(value = 1, message = UNIVERSITY_ID_MIN_ERROR) long universityId,
+        @Min(value = 1, message = DEPARTMENT_ID_MIN_ERROR) long departmentId,
+        @Email(message = INVALID_EMAIL_FORMAT) @NotBlank(message = EMAIL_BLANK_ERROR) String email,
+        @NotBlank(message = PASSWORD_BLANK_ERROR) String password,
+        @NotBlank(message = USERNAME_BLANK_ERROR) String username,
+        @NotBlank(message = STUDENT_ID_BLANK_ERROR) String studentId,
 
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-M-d", timezone = "Asia/Seoul")
-        @NotNull(message = NO_BIRTH_DATE_ERROR) LocalDate birthDate,
-        @NotNull(message = NO_GENDER_ENUM_ERROR) Gender gender
+        @NotNull(message = BIRTH_DATE_NULL_ERROR) LocalDate birthDate,
+        @NotNull(message = GENDER_NULL_ERROR) Gender gender
 ) {
-    public static final String NO_PASSWORD = "패스워드가 없습니다.";
-    public static final String NO_USERNAME = "사용자 이름이 없습니다.";
-    public static final String NO_EMAIL = "이메일이 없습니다.";
-    public static final String INVALID_EMAIL_FORMAT = "이메일 형식이 올바르지 않습니다.";
-    public static final String NO_BIRTH_DATE_ERROR = "birthDate 값이 없습니다.";
-    public static final String NO_GENDER_ENUM_ERROR = "GENDER 값이 없습니다.";
+    private static final String UNIVERSITY_ID_MIN_ERROR = "잘못된 대학 ID 값이 들어왔습니다.";
+    private static final String DEPARTMENT_ID_MIN_ERROR = "잘못된 학과 ID 값이 들어왔습니다.";
+    private static final String PASSWORD_BLANK_ERROR = "패스워드는 공백일 수 없습니다.";
+    private static final String USERNAME_BLANK_ERROR = "사용자 이름은 공백일 수 없습니다.";
+    private static final String STUDENT_ID_BLANK_ERROR = "학번은 공백일 수 없습니다.";
+    private static final String EMAIL_BLANK_ERROR = "이메일은 공백일 수 없습니다.";
+    private static final String INVALID_EMAIL_FORMAT = "이메일 형식이 올바르지 않습니다.";
+    private static final String BIRTH_DATE_NULL_ERROR = "생일 값이 없습니다.";
+    private static final String GENDER_NULL_ERROR = "성별 값이 없습니다.";
 
     public MemberJpaEntity toEntity(final String encodedPassword) {
         return MemberJpaEntity.builder()
