d0227ff62cb320225e2061598cd39a1cea990e1e
Yang JiWoong <jerry3269@ajou.ac.kr>
2024-06-03 14:18:53 +0900

    [BE] feat : 어드민 - 관리자 로그인 요청 Validation

--- backend/src/main/java/moim_today/dto/admin/auth/AdminLoginRequest.java
+++ backend/src/main/java/moim_today/dto/admin/auth/AdminLoginRequest.java
@@ -1,7 +1,12 @@
 package moim_today.dto.admin.auth;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
 public record AdminLoginRequest(
-        long universityId,
-        String adminPassword
+        @Min(value = 1, message = UNIVERSITY_ID_MIN_ERROR) long universityId,
+        @NotBlank(message = ADMIN_PASSWORD_BLANK_ERROR) String adminPassword
 ) {
+    private static final String UNIVERSITY_ID_MIN_ERROR = "잘못된 대학 ID 값이 들어왔습니다.";
+    private static final String ADMIN_PASSWORD_BLANK_ERROR = "관리자 비밀번호는 공백일 수 없습니다.";
 }
