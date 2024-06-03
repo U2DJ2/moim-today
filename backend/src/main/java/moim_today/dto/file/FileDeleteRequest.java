ce5f12a1b57c7ced5620c5df846862f32d3ee6cd
Yang JiWoong <jerry3269@ajou.ac.kr>
2024-06-03 14:24:09 +0900

    [BE] feat : 파일 삭제 요청 Validation

--- backend/src/main/java/moim_today/dto/file/FileDeleteRequest.java
+++ backend/src/main/java/moim_today/dto/file/FileDeleteRequest.java
@@ -1,7 +1,11 @@
 package moim_today.dto.file;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record FileDeleteRequest(
-        String uploadFilePath,
-        String fileName
+        @NotBlank(message = UPLOAD_FILE_PATH_BLANK_ERROR) String uploadFilePath,
+        @NotBlank(message = FILENAME_BLANK_ERROR) String fileName
 ) {
+    private static final String UPLOAD_FILE_PATH_BLANK_ERROR = "업로드 파일 경로는 공백일 수 없습니다.";
+    private static final String FILENAME_BLANK_ERROR = "파일 이름은 공백일 수 없습니다.";
 }
