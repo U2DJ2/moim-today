a9d68256865ac9d61d20bc70e83b80ec441bfb2a
Yang JiWoong <jerry3269@ajou.ac.kr>
2024-06-03 14:22:51 +0900

    [BE} feat : 비밀번호 찾기/이메일 인증 요청 Validation

--- backend/src/main/java/moim_today/dto/certification/EmailCertificationRequest.java
+++ backend/src/main/java/moim_today/dto/certification/EmailCertificationRequest.java
@@ -1,6 +1,11 @@
 package moim_today.dto.certification;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record EmailCertificationRequest(
-        String email
+        @Email(message = INVALID_EMAIL_FORMAT) @NotBlank(message = EMAIL_BLANK_ERROR) String email
 ) {
+    private static final String EMAIL_BLANK_ERROR = "이메일은 공백일 수 없습니다.";
+    private static final String INVALID_EMAIL_FORMAT = "이메일 형식이 올바르지 않습니다.";
 }
