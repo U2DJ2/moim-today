470ddf8ca9f446c45481ce264abd35b6744bf8ea
Yang JiWoong <jerry3269@ajou.ac.kr>
2024-06-03 14:23:47 +0900

    [BE] feat : 학과 추가 요청 Validation

--- backend/src/main/java/moim_today/dto/department/AddDepartmentRequest.java
+++ backend/src/main/java/moim_today/dto/department/AddDepartmentRequest.java
@@ -1,13 +1,17 @@
 package moim_today.dto.department;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import moim_today.persistence.entity.department.request_deparment.RequestDepartmentJpaEntity;
 
 @Builder
 public record AddDepartmentRequest(
-        long universityId,
-        String departmentName
+        @Min(value = 1, message = UNIVERSITY_ID_MIN_ERROR) long universityId,
+        @NotBlank(message = DEPARTMENT_NAME_BLANK_ERROR) String departmentName
 ) {
+    private static final String UNIVERSITY_ID_MIN_ERROR = "잘못된 대학 ID 값이 들어왔습니다.";
+    private static final String DEPARTMENT_NAME_BLANK_ERROR = "학과 이름은 공백일 수 없습니다.";
 
     public RequestDepartmentJpaEntity toEntity() {
         return RequestDepartmentJpaEntity.builder()
--- backend/src/main/java/moim_today/dto/department/ApproveRequestDepartmentRequest.java
+++ backend/src/main/java/moim_today/dto/department/ApproveRequestDepartmentRequest.java
@@ -1,11 +1,16 @@
 package moim_today.dto.department;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 @Builder
 public record ApproveRequestDepartmentRequest(
-        long requestDepartmentId,
-        long universityId,
-        String requestDepartmentName
+        @Min(value = 1, message = REQUEST_DEPARTMENT_ID_MIN_ERROR) long requestDepartmentId,
+        @Min(value = 1, message = UNIVERSITY_ID_MIN_ERROR) long universityId,
+        @NotBlank(message = DEPARTMENT_NAME_BLANK_ERROR) String requestDepartmentName
 ) {
+    private static final String REQUEST_DEPARTMENT_ID_MIN_ERROR = "잘못된 학과 요청 ID 값이 들어왔습니다.";
+    private static final String UNIVERSITY_ID_MIN_ERROR = "잘못된 대학 ID 값이 들어왔습니다.";
+    private static final String DEPARTMENT_NAME_BLANK_ERROR = "요청 학과 이름은 공백일 수 없습니다.";
 }
