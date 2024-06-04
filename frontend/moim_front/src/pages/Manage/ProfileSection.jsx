75289e29b5cb55ba00342fcd3267ed5232fd9504
KIM JUN YOUNG <tjclear214@gmail.com>
2024-06-04 10:51:36 +0900

    [FE] design : profile background area fix

--- frontend/moim_front/src/pages/Manage/ProfileSection.jsx
+++ frontend/moim_front/src/pages/Manage/ProfileSection.jsx
@@ -138,7 +138,7 @@ export default function ProfileSection({
   userInfo,
 }) {
   return (
-    <section className="flex flex-col w-full h-full">
+    <section className="flex flex-col w-full">
       <div className="flex flex-col self-stretch p-12 text-sm font-semibold leading-5 whitespace-nowrap bg-slate-50 rounded-[64px_0px_0px_0px] text-stone-500 max-md:px-5 max-md:mt-6 max-md:max-w-full h-full flex-grow">
         <h1 className="text-6xl font-Pretendard_Black text-black max-md:max-w-full max-md:text-4xl">
           Profile
