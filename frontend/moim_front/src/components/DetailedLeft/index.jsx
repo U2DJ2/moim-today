fa91224a96a5c3b8c11fe2981151d84d512bfcca
yurim222 <yurim@ajou.ac.kr>
2024-06-16 14:52:33 +0900

    [FE] refactor : DetailedLeft 모달창 코드 간략화

--- frontend/moim_front/src/components/DetailedLeft/index.jsx
+++ frontend/moim_front/src/components/DetailedLeft/index.jsx
@@ -4,69 +4,10 @@ import { useNavigate, useParams } from "react-router";
 
 // API
 import axios from "axios";
-
-// UI
-import { Modal } from "flowbite-react";
-import { HiOutlineExclamationCircle } from "react-icons/hi";
+import NewModal from "../NewModal";
 
 // Images
 import icon from "../../assets/svg/personIcon.svg";
-
-const modalTheme = {
-  root: {
-    base: "fixed inset-x-0 top-0 z-50 h-screen overflow-y-auto overflow-x-hidden md:inset-0 md:h-full",
-    show: {
-      on: "flex bg-gray-900 bg-opacity-50 dark:bg-opacity-80",
-      off: "hidden",
-    },
-    sizes: {
-      sm: "max-w-sm",
-      md: "max-w-md",
-      lg: "max-w-lg",
-      xl: "max-w-xl",
-      "2xl": "max-w-2xl",
-      "3xl": "max-w-3xl",
-      "4xl": "max-w-4xl",
-      "5xl": "max-w-5xl",
-      "6xl": "max-w-6xl",
-      "7xl": "max-w-7xl",
-    },
-    positions: {
-      "top-left": "items-start justify-start",
-      "top-center": "items-start justify-center",
-      "top-right": "items-start justify-end",
-      "center-left": "items-center justify-start",
-      center: "items-center justify-center",
-      "center-right": "items-center justify-end",
-      "bottom-right": "items-end justify-end",
-      "bottom-center": "items-end justify-center",
-      "bottom-left": "items-end justify-start",
-    },
-  },
-  content: {
-    base: "relative h-full w-full p-4 md:h-auto",
-    inner:
-      "relative flex max-h-[90dvh] flex-col rounded-xl bg-white shadow dark:bg-gray-700",
-  },
-  body: {
-    base: "flex-1 overflow-auto p-6",
-    popup: "pt-0",
-  },
-  header: {
-    base: "flex items-start justify-between rounded-t border-b mt-0.5 p-5 dark:border-gray-600",
-    popup: "border-b-0 p-2",
-    title:
-      "pt-0.5 text-xl font-Pretendard_SemiBold text-gray-900 dark:text-white",
-    close: {
-      base: "ml-auto inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white",
-      icon: "h-5 w-5",
-    },
-  },
-  footer: {
-    base: "flex items-center space-x-2 rounded-b border-gray-200 p-6 dark:border-gray-600",
-    popup: "border-t",
-  },
-};
 
 function DetailedLeft({
   userName,
@@ -75,9 +16,6 @@ function DetailedLeft({
   capacity,
   joined,
   image,
-  message,
-  setMessage,
-  setIsOpen,
   profileImg,
 }) {
   let { MoimId } = useParams();
@@ -85,6 +23,7 @@ function DetailedLeft({
 
   const navigate = useNavigate();
   const [isAlertModalOpen, setAlertModalOpen] = useState(false);
+  const [message, setMessage] = useState("");
 
   const body = {
     moimId: MoimId,
@@ -97,41 +36,34 @@ function DetailedLeft({
         setAlertModalOpen(true);
       })
       .catch((error) => {
-        setIsOpen(true);
+        // setIsOpen(true);
         setMessage(error.response.data.message);
+        setAlertModalOpen(true);
       });
   };
 
   return (
     <>
-      <Modal
+      <NewModal
         show={isAlertModalOpen}
         size="sm"
         onClose={() => setAlertModalOpen(false)}
-        theme={modalTheme}
-        popup
       >
-        <Modal.Header />
-        <Modal.Body>
-          <div className="text-center">
-            <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
-            <h3 className="mb-5 text-base font-Pretendard_Normal text-black">
-              {message ? message : "모임에 가입되셨습니다!"}
-            </h3>
-            <div className="flex justify-center gap-4">
-              <button
-                className="py-3 px-5 w-fit text-base font-Pretendard_Normal text-white bg-scarlet rounded-[50px] hover:cursor-pointer"
-                onClick={() => {
-                  setAlertModalOpen(false);
-                  navigate(`/join/${MoimId}`);
-                }}
-              >
-                헐랭...
-              </button>
-            </div>
-          </div>
-        </Modal.Body>
-      </Modal>
+        <h3 className="mb-5 text-base font-Pretendard_Normal text-black">
+          {message ? message : "모임에 가입되셨습니다!"}
+        </h3>
+        <div className="flex justify-center gap-4">
+          <button
+            className="py-3 px-5 w-fit text-base font-Pretendard_Normal text-white bg-scarlet rounded-[50px] hover:cursor-pointer"
+            onClick={() => {
+              setAlertModalOpen(false);
+            }}
+          >
+            확인
+          </button>
+        </div>
+      </NewModal>
+
       <div className="flex flex-col gap-4 items-center h-full w-96 max-md:w-full">
         <img className="rounded-t-2xl" src={image} />
         <div className="flex flex-col gap-2 items-center">
